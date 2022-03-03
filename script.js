84166aa641412d017e8725cca841cf96f97c0951
Eduardo Schumaher <eduardo.schumaher@gmail.com>
2022-03-03 20:10:45 -0300

    added the play round with player imput for data

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function computerPlay() {
   options = ["rock", "paper", "scissors"]
   return options[Math.floor(Math.random() * options.length)]
 }
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay()
+  playerSelection = prompt('Give a chance:').toLowerCase()
+
+  if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return ('You win! Rock beats Scissors!')
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return ('You win! Paper beats Rock!')
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return ('You win! Scissors beats Paper!')
+  } else if (playerSelection === 'scissors' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'rock') {
+    return ('Draw')
+  } else
+    return ('You lose!')
+}
+
+console.log(playRound())
