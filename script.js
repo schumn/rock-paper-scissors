7df8800f3fcdbdad42149d7a6ae257b8b2df0e67
Eduardo Schumaher <eduardo.schumaher@gmail.com>
2022-03-14 23:15:27 -0300

    added the game function and a score counter

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0
+
+let computerScore = 0
+
 function computerPlay() {
   options = ["rock", "paper", "scissors"]
   return options[Math.floor(Math.random() * options.length)]
@@ -8,15 +12,32 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = prompt('Give a chance:').toLowerCase()
 
   if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore++
     return ('You win! Rock beats Scissors!')
+
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++
     return ('You win! Paper beats Rock!')
+
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++
     return ('You win! Scissors beats Paper!')
+
   } else if (playerSelection === 'scissors' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'rock') {
     return ('Draw')
+
   } else
-    return ('You lose!')
+    computerScore++
+  return ('You lose!')
 }
 
-console.log(playRound())
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(`Game ${i}: ${playRound()}`)
+  }
+  if (playerScore > computerScore) {
+    console.log(`You win the game! ${playerScore} x ${computerScore}`)
+  } else {
+    console.log(`You lose the game! ${playerScore} x ${computerScore}`)
+  }
+}
